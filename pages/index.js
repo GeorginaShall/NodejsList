ef7a7646a70faa2c0b1231b899dc77ceac0947d4
gugushall <107054293+gugushall@users.noreply.github.com>
2023-01-24 20:59:56 +0200

    1

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     
       <div>
 
-      <h1 className={styles.title}>Homepage</h1>
+      <h1 className={styles.title}>HomePage</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <Link href="/students/" className={styles.btn}>See Students Listing</Link>
