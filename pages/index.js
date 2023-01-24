b4bcc0079899b8b17c4a89bcc851a8e9468b43d4
gugushall <107054293+gugushall@users.noreply.github.com>
2023-01-24 20:26:01 +0200

    initiall

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+    <>
+      <Head>
+        <title>students List | HomePage</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+
+    
+      <div>
+
+      <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <Link href="/students/" className={styles.btn}>See Students Listing</Link>
+
+
+      </div>
+    </>
+
+  );
+}
